86bbc01ab08779403fe2b299775bda2fac7bc771
Korpan-Artem <55976064+Korpan-Artem@users.noreply.github.com>
2022-10-22 20:50:38 +0300

    Slick slider (#3)
    * 211022 #3
    
    * added swipper
    
    * 221022 #2
    
    * 221022 #4

--- src/components/SliderCharacteristics/SliderCharacteristics.jsx
+++ src/components/SliderCharacteristics/SliderCharacteristics.jsx
@@ -3,39 +3,15 @@ import { Swiper,SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
+import SliderCharacteristicsItem from "./SliderCharacteristicsItem";
+
 
 
 
 const SliderCharacteristics = ({data}) => {
   return (
     <>
-      <div>
-        {
-          !!data &&
-          <Swiper
-            pagination={{
-              type: "fraction",
-            }}
-            navigation={true}
-            initialSlide={0}
-            slidesPerView={1}
-            modules={[Pagination, Navigation]}
-            className="mySwiper"
-          >
-              {!!data && data.map((item,index) => (
-                <SwiperSlide key={index}>
-                <div className="three-itempage__max-item three-itempage__max-item-characteristics">
-                  <img
-                    src={!!item?.localFile?.url && item?.localFile?.url}
-                    className="three-itempage__max-item-img"
-                    alt={""}
-                  />
-                  {/*<div className={"number-slide"}>{`${index+1}/${data.length}`}</div>*/}
-                </div>
-                </SwiperSlide>
-              ))}
-          </Swiper>
-        }
+       <SliderCharacteristicsItem data={data} initialSlide={0}/>
       </div>
     </>
   )
